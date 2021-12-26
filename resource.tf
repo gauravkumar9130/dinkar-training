411c74bb81993101aa465fc023d8e9193bfbf3af
gauravkumar9130 <55126211+gauravkumar9130@users.noreply.github.com>
2021-12-26 21:01:09 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -11,3 +11,9 @@ provider "azurerm" {
   name     = "gauravrg"
   location = "West Europe"
 }
+
+ resource "azurerm_resource_group" "example3" {
+  name     = "gauravrg1"
+  location = "West Europe"
+}
+
